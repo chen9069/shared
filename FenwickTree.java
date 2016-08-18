55467c99c1d00f7b70a942757ea86086705ca89b
chen9069 <cj815@nyu.edu>
2016-08-18 14:15:39 -0700

    Update FenwickTree.java

--- FenwickTree.java
+++ FenwickTree.java
@@ -2,8 +2,7 @@ import java.util.*;
 public class FenwickTree {
     public long count = 0;
     /**
-     * Start from index+1 if you updating index in original array. Keep adding this value 
-     * for next node till you reach outside range of tree
+     * binaryIndexedTree start with index 1.
      */
     public void updateBinaryIndexedTree(int binaryIndexedTree[], int val, int index){
         while(index < binaryIndexedTree.length){
@@ -54,22 +53,10 @@ public class FenwickTree {
 	}
     }
     
-    /**
-     * To get parent
-     * 1) 2's complement to get minus of index
-     * 2) AND this with index
-     * 3) Subtract that from index
-     */
     private int getParent(int index){
 	return index - (index & -index);
     }
     
-    /**
-     * To get next
-     * 1) 2's complement of get minus of index
-     * 2) AND this with index
-     * 3) Add it to index
-     */
     private int getNext(int index){
 	return index + (index & -index);
     }
@@ -78,24 +65,24 @@ public class FenwickTree {
         Random random = new Random();
 	int size = Integer.parseInt(args[0]); 
 	int input[] = new int[size];
-	int binaryIndexedTree[] = new int[size+1];
+	int bit[] = new int[size+1];
 	int bit2[] = new int[size+1];
 	for (int i = 0; i < input.length; i ++) {
 		input[i] = random.nextInt(16) - 8;
 	}
         FenwickTree ft = new FenwickTree();
 	long start = System.currentTimeMillis();
-        ft.createTree(input, binaryIndexedTree);
+        ft.createTree(input, bit);
 	long duration = System.currentTimeMillis() - start;
 //	System.out.println(Arrays.toString(input));
 	System.out.println(String.format("Count: %d", ft.count));
-//	System.out.println(Arrays.toString(binaryIndexedTree));
+//	System.out.println(Arrays.toString(bit));
 	System.out.println(String.format("Scale: %f", ((double) ft.count) / ((double) size))); 
     	/*********************************/
 	ft.createTree2(input, bit2);
 	for (int i = 1; i <= size; i ++) {
-	    if (binaryIndexedTree[i] != bit2[i])
-		System.out.println(i);
+	    if (bit[i] != bit2[i])
+		System.err.println("Wrong!" + i);
 	}
 	/*********************************/
 //	System.out.println(Arrays.toString(bit2));
